71b4be19c57436ac3c9fa42ffa9fc54c62e1f0eb
otakakot <kotaro.otaka@gmail.com>
2023-12-21 23:21:00 +0900

    first commit

--- script/enable.sh
+++ script/enable.sh
@@ -6,16 +6,12 @@ while read line; do
 
     count=$(($count + 1))
 
-    if [[ ${line:0:1} == "#" ]]; then
-        continue
-    fi
-
     if [[ $count -eq 5 ]] ; then
-        sed -i -e "5s:^:#:" $1
+        sed -i -e '5s/^#//' $1
     fi
 
     if [[ $count -eq 6 ]]; then
-        sed -i -e "6s:^:#:" $1
+        sed -i -e '6s/^#//' $1
     fi
 
 done < $1
